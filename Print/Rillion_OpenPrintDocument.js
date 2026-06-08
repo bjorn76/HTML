6eee40521dabe7150b918b4d92757a481b4b92e8
bjorn76 <bjorn.k.j@gmail.com>
2026-06-08 21:17:00 +0200

    Rillion_Workaround

---
+++ Print/Rillion_OpenPrintDocument.js
@@ -0,0 +1,76 @@
+function OpenPrintDocument() {
+                    window.open('./DocumentHandlers/PurchaseOrderHtml.ashx?action=print', '', 'scrollbars=yes,menubar=no,height=400,width=400,resizable=yes,toolbar=no,location=no,status=no');
+                    __doPostBack('__Page', 'print')
+                }
+                var __cultureInfo = {
+                    "name": "sv-SE",
+                    "numberFormat": {
+                        "CurrencyDecimalDigits": 2,
+                        "CurrencyDecimalSeparator": ",",
+                        "IsReadOnly": false,
+                        "CurrencyGroupSizes": [3],
+                        "NumberGroupSizes": [3],
+                        "PercentGroupSizes": [3],
+                        "CurrencyGroupSeparator": " ",
+                        "CurrencySymbol": "kr",
+                        "NaNSymbol": "NaN",
+                        "CurrencyNegativePattern": 8,
+                        "NumberNegativePattern": 1,
+                        "PercentPositivePattern": 0,
+                        "PercentNegativePattern": 0,
+                        "NegativeInfinitySymbol": "-∞",
+                        "NegativeSign": "-",
+                        "NumberDecimalDigits": 2,
+                        "NumberDecimalSeparator": ",",
+                        "NumberGroupSeparator": ".",
+                        "CurrencyPositivePattern": 3,
+                        "PositiveInfinitySymbol": "∞",
+                        "PositiveSign": "+",
+                        "PercentDecimalDigits": 2,
+                        "PercentDecimalSeparator": ",",
+                        "PercentGroupSeparator": " ",
+                        "PercentSymbol": "%",
+                        "PerMilleSymbol": "‰",
+                        "NativeDigits": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+                        "DigitSubstitution": 1
+                    },
+                    "dateTimeFormat": {
+                        "AMDesignator": "",
+                        "Calendar": {
+                            "MinSupportedDateTime": "\/Date(-62135596800000)\/",
+                            "MaxSupportedDateTime": "\/Date(253402300799999)\/",
+                            "AlgorithmType": 1,
+                            "CalendarType": 1,
+                            "Eras": [1],
+                            "TwoDigitYearMax": 2049,
+                            "IsReadOnly": false
+                        },
+                        "DateSeparator": "-",
+                        "FirstDayOfWeek": 1,
+                        "CalendarWeekRule": 2,
+                        "FullDateTimePattern": "\u0027den \u0027d MMMM yyyy HH:mm:ss",
+                        "LongDatePattern": "\u0027den \u0027d MMMM yyyy",
+                        "LongTimePattern": "HH:mm:ss",
+                        "MonthDayPattern": "\u0027den \u0027d MMMM",
+                        "PMDesignator": "",
+                        "RFC1123Pattern": "ddd, dd MMM yyyy HH\u0027:\u0027mm\u0027:\u0027ss \u0027GMT\u0027",
+                        "ShortDatePattern": "yyyy-MM-dd",
+                        "ShortTimePattern": "HH:mm:ss",
+                        "SortableDateTimePattern": "yyyy\u0027-\u0027MM\u0027-\u0027dd\u0027T\u0027HH\u0027:\u0027mm\u0027:\u0027ss",
+                        "TimeSeparator": ":",
+                        "UniversalSortableDateTimePattern": "yyyy\u0027-\u0027MM\u0027-\u0027dd HH\u0027:\u0027mm\u0027:\u0027ss\u0027Z\u0027",
+                        "YearMonthPattern": "MMMM yyyy",
+                        "AbbreviatedDayNames": ["sön", "mån", "tis", "ons", "tor", "fre", "lör"],
+                        "ShortestDayNames": ["sö", "må", "ti", "on", "to", "fr", "lö"],
+                        "DayNames": ["söndag", "måndag", "tisdag", "onsdag", "torsdag", "fredag", "lördag"],
+                        "AbbreviatedMonthNames": ["jan", "feb", "mar", "apr", "maj", "jun", "jul", "aug", "sep", "okt", "nov", "dec", ""],
+                        "MonthNames": ["januari", "februari", "mars", "april", "maj", "juni", "juli", "augusti", "september", "oktober", "november", "december", ""],
+                        "IsReadOnly": false,
+                        "NativeCalendarName": "Gregoriansk kalender",
+                        "AbbreviatedMonthGenitiveNames": ["jan", "feb", "mar", "apr", "maj", "jun", "jul", "aug", "sep", "okt", "nov", "dec", ""],
+                        "MonthGenitiveNames": ["januari", "februari", "mars", "april", "maj", "juni", "juli", "augusti", "september", "oktober", "november", "december", ""]
+                    },
+                    "eras": [1, "A.D.", null, 0]
+                };
+                //]]>
+            </script>
